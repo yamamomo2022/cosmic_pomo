fca6035eed8f24b8c459d28f50ee394a58cc0520
viole <violetraisondetre@gmai.com>
2025-04-22 07:51:34 +0900

    feat: integrate Firebase and update project configuration
    - Added Firebase configuration files to .gitignore
    - Created firebase.json for Firebase project settings
    - Updated Podfile.lock to include Firebase dependencies
    - Modified project.pbxproj to reference GoogleService-Info.plist
    - Initialized Firebase in main.dart
    - Updated pubspec.yaml to include firebase_core dependency
    - Updated pubspec.lock with firebase_core and related packages

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
 
+import 'firebase_options.dart';
 import 'screens/pomodoro_screen.dart';
 import 'utils/logger.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
   // Google Mobile Ads SDKを初期化する
   await MobileAds.instance.initialize();

24df18fa0d7a2579595e50e2375ea8024e1076f7
viole <violetraisondetre@gmai.com>
2025-04-26 06:44:21 +0900

    feat: update Podfile and integrate Firebase Analytics
    - Updated iOS platform version in Podfile from 12.0 to 13.0.
    - Modified Podfile.lock to include Firebase Analytics dependencies.
    - Added Firebase Analytics instance and observer in main.dart.
    - Updated pubspec.yaml to include firebase_analytics and hooks_riverpod dependencies.

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,21 @@
+import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 import 'firebase_options.dart';
 import 'screens/pomodoro_screen.dart';
 import 'utils/logger.dart';
+
+/// FirebaseAnalyticsのインスタンス
+final analyticsRepository = Provider((ref) => FirebaseAnalytics.instance);
+
+/// FirebaseAnalyticsObserverのインスタンス
+final analyticsObserverRepository = Provider(
+  (ref) => FirebaseAnalyticsObserver(analytics: ref.watch(analyticsRepository)),
+);
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
